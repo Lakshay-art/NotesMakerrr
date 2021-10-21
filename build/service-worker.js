5ad6168cbe9717064ea714b9cb95add996ddd897
Lakshay Gupta <lakshay0173.cse19@chitkara.edu.in>
2021-10-21 20:12:17 +0530

    changed advertisement

--- build/service-worker.js
+++ build/service-worker.js
@@ -1,1 +1,38 @@
-self.addEventListener('fetch', function(event) {});
+importScripts('/cache-polyfill.js');
+
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('NotesMakerrr').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/index.html?homescreen=1',
+       '/?homescreen=1',
+       "main.css",
+       "main.js",
+       "main.js.map",
+       "runtime-main.js",
+       "runtime-main.js.map",
+       "static/js/2.0405f225.chunk.js",
+       "static/js/2.0405f225.chunk.js.map",
+       "static/js/3.dbe2aaf5.chunk.js",
+       "static/js/3.dbe2aaf5.chunk.js.map",
+      
+       "static/css/main.c366c98a.chunk.css.map",
+       "static/js/2.0405f225.chunk.js.LICENSE.txt"
+    
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(event) {
+    console.log(event.request.url);
+   
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+   });
